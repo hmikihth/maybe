93af835c79c2f1775fa739eecdbab10a26988a9e
hmikihth <szubotaj@gmail.com>
2016-04-03 05:07:25 +0100

    I think better to use the unittest module. Maybe tomorrow I write tests if I will have time.

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,4 +2,4 @@ from common import maybe
 
 
 def test_no_operations():
-    assert maybe("true") == "maybe has not detected any file system operations from true."
+    assert maybe("true") == _("maybe has not detected any file system operations from true.")
