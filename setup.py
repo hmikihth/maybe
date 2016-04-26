fa3ac5af1a6967952814f7a1f00292fdbe21cd68
hmikihth <szubotaj@gmail.com>
2016-04-26 01:43:02 +0100

    Bash completion has been added. The mo file creation has been automatized.

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
-# Based on setup.py from https://github.com/pypa/sampleproject
 import os
-from setuptools import setup
+import glob
 from distutils.command.build import build
 from distutils.command.install_data import install_data
+from setuptools import setup
 import subprocess
-import glob
 
 PO_DIR = "po"
 MO_DIR = "share/locale"
 BASH_COMP_DIR = "/etc/bash_completion.d"
+
+datafiles = []
+datafiles.append((BASH_COMP_DIR, ["maybe_bc"]))
+
+for po in glob.glob (os.path.join (PO_DIR, '*.po')):
+            lang = os.path.basename(po[:-3])
+            mo = os.path.join('build', MO_DIR, lang, 'LC_MESSAGES', 'maybe.mo')
+            print (mo)
+            datafiles.append((MO_DIR, [mo]))
+            directory = os.path.dirname(mo)
+            if not os.path.exists(directory):
+                print('creating %s' % directory)
+                os.makedirs(directory)
+
+            print('compiling %s -> %s' % (po, mo))
+            try:
+                rc = subprocess.call(['msgfmt', '-o', mo, po])
+                if rc != 0:
+                    raise "Warning, msgfmt returned %d" % rc
+            except:
+                    print ("Building gettext files failed.")
+                    print ("%s: %s" % (type(e), e))
+                    sys.exit(1)
 
 datafiles = []
 datafiles.append((BASH_COMP_DIR, ["maybe_bc"]))
@@ -90,6 +112,6 @@ setup(
             "maybe = maybe.maybe:main",
         ],
     },
-        
+
     data_files=datafiles,
 )
